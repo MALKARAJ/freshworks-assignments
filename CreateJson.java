6b213dc3378d210822ba8e5734478f2db9e4b475
MALKARAJ <62066377+MALKARAJ@users.noreply.github.com>
2021-01-01 20:47:13 +0530

    Add files via upload

---
+++ CreateJson.java
@@ -0,0 +1,216 @@
+import java.io.*;
+import java.time.LocalDateTime;
+import java.util.*;
+
+public class CreateJson {
+	
+	
+	
+/* update*/
+	public static void update(String key)throws IOException
+	{
+		BufferedReader br=new BufferedReader(new FileReader("example.json"));
+   	  
+		  
+		 String line=br.readLine().trim();
+		 br.close();
+		 String valueFromFile=",";
+		 
+		 if((valueFromFile=(isKeyExist(line,key)))!=null)
+		 {
+			
+			 //System.out.println(valueFromFile);
+line=line.replaceAll("\\{\""+key+"\""+":\\"+valueFromFile.substring(0,valueFromFile.length()-1)+"\\}\\}","");	
+line=line.replaceAll(",\""+key+"\""+":\\"+valueFromFile.substring(0,valueFromFile.length()-1)+"\\}", "");
+line=line.replaceAll("\""+key+"\""+":\\"+valueFromFile.substring(0,valueFromFile.length()-1)+"\\},","");
+//System.out.println("\\{\\{\""+key+"\""+":\\"+valueFromFile.substring(0,valueFromFile.length()-1)+"\\}\\}");
+
+//C:\\Users\\LENOVO\\eclipse-workspace\\JsonWrite\\
+File f=new File("example.json");
+FileWriter fp=new FileWriter(f);
+fp.write(line);
+fp.close();
+		
+		
+	}
+		 }
+	
+public static void  delete(String key) throws Exception
+	{
+		  BufferedReader br=new BufferedReader(new FileReader("example.json"));
+     	  
+		  
+			 String line=br.readLine().trim();
+			 br.close();
+			 String valueFromFile=",";
+			 
+			 if((valueFromFile=(isKeyExist(line,key)))!=null)
+			 {
+				 
+				 if(valueFromFile.contains("TIMETOLIVE"))
+				 {
+					 if((LocalDateTime.now().toString()).compareTo(valueFromFile.substring(14,42))>=0)
+					 {
+				 System.out.println("TIME-TO_LIVE for the key got expired no such key exist ");
+					 }
+					 else
+				      System.out.println("Value deleted successfully");
+				 }
+				 else
+					 System.out.println("Value deleted successfully");
+				 update(key);
+			 }
+			 else
+				 System.out.println("key doesnt exist");
+/*
+System.out.println(valueFromFile);
+line=line.replaceAll("\\{\""+key+"\""+":\\"+valueFromFile.substring(0,valueFromFile.length()-1)+"\\}\\}","");	
+line=line.replaceAll(",\""+key+"\""+":\\"+valueFromFile.substring(0,valueFromFile.length()-1)+"\\}", "");
+line=line.replaceAll("\""+key+"\""+":\\"+valueFromFile.substring(0,valueFromFile.length()-1)+"\\},","");
+  System.out.println("\\{\\{\""+key+"\""+":\\"+valueFromFile.substring(0,valueFromFile.length()-1)+"\\}\\}");
+
+	File f=new File("C:\\Users\\LENOVO\\eclipse-workspace\\JsonWrite\\example.json");
+	FileWriter fp=new FileWriter(f);
+	fp.write(line);
+	fp.close();
+	System.out.println("corresponding value for the key is delete successfully");
+			 }
+			 else
+				 System.out.println("Key does not exist");
+*/
+		 }
+
+		  
+  		  	
+	
+	
+	public static void read(String key) throws IOException
+	{
+		
+		  BufferedReader br=new BufferedReader(new FileReader("example.json"));
+			     	  
+		 String line=br.readLine();
+		 String valueFromFile="";
+		 if((valueFromFile=(isKeyExist(line,key)))!=null)
+		 {
+			 if(valueFromFile.contains("TIMETOLIVE"))
+			 {
+				 if((LocalDateTime.now().toString()).compareTo(valueFromFile.substring(14,42))>=0)
+				 {
+					 System.out.println("TIME-TO_LIVE for the key got expired no such key exist found");
+				    update(key);
+				 }
+				 else
+					 System.out.println(valueFromFile);
+				 
+			}
+			 else 
+			 System.out.println(valueFromFile);
+		 }
+		 else
+			 System.out.println("Key does not exist");
+			    	
+			    
+}
+
+
+	
+	public  static String isKeyExist(String line,String key)
+	{
+	
+		String keyappend="";
+		String valueFromFile="";
+		for(int i=0;i<line.length()-1;i++)
+		{
+			if(line.charAt(i)!='"'&&line.charAt(i)!='{'&&line.charAt(i)!=':' &&line.charAt(i)!=',')
+			keyappend=keyappend+line.charAt(i);
+			
+			if(line.charAt(i)==':'&&line.charAt(i+1)=='{')
+			{ 
+				if(keyappend.equals(key)) {
+				i++;
+			
+					while(line.charAt(i)!='}')
+					{
+						valueFromFile=valueFromFile+line.charAt(i);
+						i++;
+					}
+					valueFromFile=valueFromFile+line.charAt(i);
+					return valueFromFile;
+				}
+				else
+				{
+					//System.out.println(keyappend);
+					keyappend="";
+					while(line.charAt(i)!='}')
+					{
+						i++;
+					}
+				
+				}
+				
+			}
+		}
+		
+		return null;
+		
+			
+}
+
+	public static void  create(String key,Object value,int valueType)  throws IOException {
+		
+		
+	  BufferedReader br=new BufferedReader(new FileReader("example.json"));
+		      //System.out.println("create"+path);   
+	 String line=br.readLine();
+			  if(line==null)
+			  {
+				  //C:\\Users\\LENOVO\\eclipse-workspace\\JsonWrite\\
+					File f=new File("example.json");
+					 
+					FileWriter fp=new FileWriter(f);	
+					  if(valueType==1||valueType==4)
+						  fp.write("{"+"\""+key+"\""+":"+value+"}");
+					  else
+						  if(valueType==2)
+							  fp.write("{"+"\""+key+"\""+":"+"\""+value+"\""+"}");
+						
+					 
+					  fp.close();
+					
+			  }
+			  else
+			  {
+			   if(isKeyExist(line,key)==null) {
+			  br.close();
+			  
+//			  System.out.println(line.substring(0,line.length()-1));
+			  //C:\\Users\\LENOVO\\eclipse-workspace\\JsonWrite\\
+			  
+			  File f=new File("example.json");
+			  FileWriter fp=new FileWriter(f);	
+			  String ans="";
+			  if(valueType==1||valueType==4)
+			  {
+				  ans=line.substring(0,line.length()-1)+","+"\""+key+"\""+":"+value+"}";
+			  }
+			  else {
+				  ans=line.substring(0,line.length()-1)+","+"\""+key+"\""+":"+"\""+value+"\""+"}";
+			  	}
+			  fp.write(ans);
+			  fp.close();
+	//		  System.out.println(line.substring(0,line.length()-1)+","+"\""+key+"\""+":"+"\""+value+"\""+"}");
+ }
+			   else
+			   {
+				   
+				   System.out.println("oops key already exist");
+			   }
+			
+			 }
+			  
+		
+		
+	
+	}
+}
---
+++ DriverProgram.java
@@ -0,0 +1,103 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.time.LocalDateTime;
+import java.util.Scanner;
+
+public class DriverProgram extends CreateJson {
+
+	
+	public static void main(String[] args) throws Exception {
+	  String path="";
+		String c="";
+		int startflag=0;
+		String timeLive;
+	    while(true) {
+		Scanner scan=new Scanner(System.in);
+		
+		System.out.println("1.create\n2.read\n3.delete\n4.exit");
+		if(startflag==0)
+		{
+	/*		System.out.println("do you want to give path for the file(y/n)");
+			if(scan.next().equals("y"))
+				{
+			  	 System.out.println("Enter the path");
+				 path=scan.next();
+				 
+				}
+			else
+				path="example.json";
+		*/	
+		}
+	
+		startflag++;
+		System.out.println("now enter the option");
+		String input=scan.next();
+		 
+		 switch(input)
+		 {
+		 
+		 	case "1":
+		 	{
+			 
+			 System.out.println("enter the key");
+			 String key=scan.next();
+			 System.out.println("Do you want to create time to live property for key(y/n)");
+			   timeLive=scan.next();
+			 if((timeLive.trim()).equals("y"))
+			 {
+				 System.out.println("Enter the number of second key wants to live(integer value)");
+				int noOfSeconds= scan.nextInt();
+				 c=""+"{\""+"TIMETOLIVE\":"+LocalDateTime.now().plusSeconds(noOfSeconds);
+				 
+				 
+			 }
+			 
+			 System.out.println("press 4 for json object ");
+			 int opt=scan.nextInt();
+			 System.out.println("dont leave any spaces string will terminate and they enclose the string with double quotes");
+			 System.out.println("syntax is {\"Key\":value,\"Key\":value,..}");
+			 System.out.println("please type like above systax now ");
+			
+			 if(opt==4)
+					 {
+						 System.out.println("enter the value as jsonObject");
+				         String value="";
+						 if(timeLive.equals("y"))
+				create(key,c+scan.next().replaceAll("\\s","").replaceFirst("\\{",","),opt);
+						 else
+				        create(key,scan.next().replaceAll("\\s", ""),opt);
+						 
+							 
+					 }
+	 		 break;
+		 	}
+		 	case "2":
+		 	{
+		 		System.out.println("Enter the key to get the value as object");
+		 		read(scan.next());
+			 break;
+		 	}
+		 	case "3":
+		 	{
+		 		System.out.println("enter the key to delete the value as object");
+		 		delete(scan.next());
+		      break;		
+		  	}
+		 	case "4":
+		 	{
+		 		System.out.println("|thank you welcom|");
+		 		return;
+		 	}
+		 
+		 }
+			  
+			   
+		 
+		 }
+	     
+		
+	}
+
+}
+
+
